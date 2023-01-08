e970b82e9dbb67eabcaf427180f8e414c6b48190
isouravkr <107295325+isouravkr@users.noreply.github.com>
2023-01-08 20:53:08 +0530

    Add files via upload

---
+++ mex of 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   int a,b,x=0;
+   cout<<"enter two numbers:\n ";
+  cin>>a>>b;
+   if (x != a && x != b)
+      x = b+1;
+  cout<<"Mex of 2 \n";
+  cout<<x;
+}
